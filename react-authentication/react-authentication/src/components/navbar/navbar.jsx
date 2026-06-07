8ba75c78987a8d13b6ea18389b5bd59b6f13bc89
Azizullah <azizullahcodes@gmail.com>
2026-06-07 17:14:05 +0500

    navbar.css

--- react-authentication/react-authentication/src/components/navbar/navbar.jsx
+++ react-authentication/react-authentication/src/components/navbar/navbar.jsx
@@ -1,27 +1,85 @@
-//navbar.jsx
-import React from "react";
+// //navbar.jsx
+// import React from "react";
+// import { Link } from "react-router-dom";
+// const Navbar = ()=>{
+//     return(
+//         <>
+//       <ul>
+//         <li>
+//             <Link to='/'> Home</Link>
+//            </li>
+
+//            <li>
+//             <Link to='/about'> About</Link>
+//            </li>
+
+//            <li>
+//             <Link to='/sign-up'> Sign Up</Link>
+//            </li>
+
+//            <li>
+//             <Link to='/login'> Log In</Link>
+//            </li>
+//       </ul>
+//         </>
+//     )
+// }
+// export default Navbar;
+
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-const Navbar = ()=>{
-    return(
-        <>
-      <ul>
-        <li>
-            <Link to='/'> Home</Link>
-           </li>
+import "./navbar.css";
 
-           <li>
-            <Link to='/about'> About</Link>
-           </li>
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-           <li>
-            <Link to='/sign-up'> Sign Up</Link>
-           </li>
+  return (
+    <nav className="navbar">
 
-           <li>
-            <Link to='/login'> Log In</Link>
-           </li>
-      </ul>
-        </>
-    )
-}
+      <div className="nav-container">
+
+        {/* Logo */}
+        <Link to="/" className="logo">
+          Auth<span>Flow</span>
+        </Link>
+
+        {/* Hamburger */}
+        <div
+          className={`hamburger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Nav Links */}
+        <ul className={`nav-links ${menuOpen ? "show" : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+
+         
+
+          <div className="nav-buttons">
+            <Link to="/login" className="login-btn">
+              Log In
+            </Link>
+
+            <Link to="/sign-up" className="signup-btn">
+              Sign Up
+            </Link>
+          </div>
+        </ul>
+
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
