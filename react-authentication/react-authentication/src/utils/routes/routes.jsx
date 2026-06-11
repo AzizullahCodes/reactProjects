45aca2e78fb9d2a721a93df1a2c9599d73b158a0
Azizullah <azizullahcodes@gmail.com>
2026-06-11 10:53:08 +0500

    update

--- react-authentication/react-authentication/src/utils/routes/routes.jsx
+++ react-authentication/react-authentication/src/utils/routes/routes.jsx
@@ -1,30 +1,82 @@
-//routes.jsx
+// //routes.jsx
+// import React from "react";
+// import {Routes,Route} from 'react-router-dom';
+// import SignUp from "../../pages/sign-up/sign-up";
+// import LogIn from "../../pages/log-in/login";
+// import Home from "../../pages/home/home";
+// import About from "../../pages/about/about";
+// import Contact from "../../pages/contact/contact";
+// import PublicRoutes from "../public-routes/public-routes";
+// import ProtectedRoutes from "../protected-routes/protected-routes";
+// const AppRoutes = ()=>{
+//     return(
+//         <Routes>
+//             {/* public-routes */}
+//             <Route element={<PublicRoutes/>}>
+//             <Route path="/sign-up" element={<SignUp/>}/>
+//             <Route path="/login" element={<LogIn/>}/>
+
+//             </Route>
+//             {/* protected-routes */}
+//             <Route element={<ProtectedRoutes/>}>
+//  <Route path="/" element={<Home/>}/>
+//             <Route path="/about" element={<About/>}/>
+//             <Route path="/contact" element={<Contact/>}/>
+//             </Route>
+            
+//         </Routes>
+//     )
+// }
+// export default AppRoutes;
+
+
 import React from "react";
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
 import SignUp from "../../pages/sign-up/sign-up";
 import LogIn from "../../pages/log-in/login";
 import Home from "../../pages/home/home";
 import About from "../../pages/about/about";
 import Contact from "../../pages/contact/contact";
+
+// Route Guards
 import PublicRoutes from "../public-routes/public-routes";
 import ProtectedRoutes from "../protected-routes/protected-routes";
-const AppRoutes = ()=>{
-    return(
-        <Routes>
-            {/* public-routes */}
-            <Route element={<PublicRoutes/>}>
-            <Route path="/sign-up" element={<SignUp/>}/>
-            <Route path="/login" element={<LogIn/>}/>
 
-            </Route>
-            {/* protected-routes */}
-            <Route element={<ProtectedRoutes/>}>
- <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            </Route>
-            
-        </Routes>
-    )
-}
+const AppRoutes = () => {
+  const isLoggedIn = localStorage.getItem("user");
+
+  return (
+    <Routes>
+
+      {/* PUBLIC ROUTES (only for non-logged users) */}
+      <Route element={<PublicRoutes />}>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+      </Route>
+
+      {/* PROTECTED ROUTES (only for logged users) */}
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+
+      {/* ✅ CATCH-ALL ROUTE (IMPORTANT FIX) */}
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <Navigate to="/" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
+        }
+      />
+
+    </Routes>
+  );
+};
+
 export default AppRoutes;
