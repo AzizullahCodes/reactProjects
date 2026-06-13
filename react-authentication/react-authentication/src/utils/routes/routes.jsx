3b85cca00ec13a6ddcc67013900080f042b5f747
Azizullah <azizullahcodes@gmail.com>
2026-06-13 19:11:21 +0500

    upd

--- react-authentication/react-authentication/src/utils/routes/routes.jsx
+++ react-authentication/react-authentication/src/utils/routes/routes.jsx
@@ -1,34 +1,3 @@
-// //routes.jsx
-// import React from "react";
-// import {Routes,Route} from 'react-router-dom';
-// import SignUp from "../../pages/sign-up/sign-up";
-// import LogIn from "../../pages/log-in/login";
-// import Home from "../../pages/home/home";
-// import About from "../../pages/about/about";
-// import Contact from "../../pages/contact/contact";
-// import PublicRoutes from "../public-routes/public-routes";
-// import ProtectedRoutes from "../protected-routes/protected-routes";
-// const AppRoutes = ()=>{
-//     return(
-//         <Routes>
-//             {/* public-routes */}
-//             <Route element={<PublicRoutes/>}>
-//             <Route path="/sign-up" element={<SignUp/>}/>
-//             <Route path="/login" element={<LogIn/>}/>
-
-//             </Route>
-//             {/* protected-routes */}
-//             <Route element={<ProtectedRoutes/>}>
-//  <Route path="/" element={<Home/>}/>
-//             <Route path="/about" element={<About/>}/>
-//             <Route path="/contact" element={<Contact/>}/>
-//             </Route>
-            
-//         </Routes>
-//     )
-// }
-// export default AppRoutes;
-
 
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
