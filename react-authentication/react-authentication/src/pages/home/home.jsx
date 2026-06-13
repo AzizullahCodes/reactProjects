442ef735c5d0ffd8586c288b8a465b04cbb8a358
Azizullah <azizullahcodes@gmail.com>
2026-06-13 17:25:02 +0500

    upd

--- react-authentication/react-authentication/src/pages/home/home.jsx
+++ react-authentication/react-authentication/src/pages/home/home.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import Swal from 'sweetalert2'; 
 import { useNavigate } from "react-router-dom";
 const Home = ()=>{
   const navigate = useNavigate('')
   const logOutHandler = ()=>{
+  localStorage.removeItem('loggedInUser')
+  //swal alert
+   Swal.fire({
+        title: 'You have logged out successfully!',
+        icon: 'success',
+        draggable: true,
+      
+       
+      }).then(()=>{
+        navigate('/login')
+      })
+ 
+  }
+  //delete account 
+  const deleteAccountFun = ()=>{
     let users = JSON.parse(localStorage.getItem('users'));
   let loggedUser = JSON.parse(localStorage.getItem('loggedInUser'));
   console.log(loggedUser)
@@ -12,12 +28,22 @@ const Home = ()=>{
   users.splice(requiredIndex,1);
   localStorage.setItem('users',JSON.stringify(users))
   localStorage.removeItem('loggedInUser')
-  navigate('/login')
+  //swal alert
+   Swal.fire({
+        title: 'Your account has been deleted permanently!',
+        icon: 'error',
+        draggable: true,
+      }).then(()=>{
+        navigate('/login')
+      })
+ 
   }
   return(
     <>
     <h1>Home page</h1>
     <button onClick={logOutHandler}>log out</button>
+    <br/>
+    <button onClick={deleteAccountFun}>delete account</button>
     </>
   )
 }
