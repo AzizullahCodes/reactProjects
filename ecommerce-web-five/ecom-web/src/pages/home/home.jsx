86a26a05e2d682660cec34d7ccb66e754bc37dc5
Azizullah <azizullahcodes@gmail.com>
2026-06-21 22:55:24 +0500

    upd

--- ecommerce-web-five/ecom-web/src/pages/home/home.jsx
+++ ecommerce-web-five/ecom-web/src/pages/home/home.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import mobileImage from './images/mobileImage.jfif';
 import laptopImage from './images/laptopImage.jfif';
 import smartwatchImage from './images/smartwatchImage.jfif';
 import cameraImage from './images/cameraImage.jfif';
 import keyboardImage from './images/keyboardImage.jfif';
 const Home = ()=>{
+    const allImages = [mobileImage,laptopImage,smartwatchImage,cameraImage,keyboardImage];
+    const [index,setIndex] = useState(0);
+    useEffect(()=>{
+
+    let a = setInterval(() => {
+        setIndex(index + 1);
+        if(index == allImages.length - 1){
+            setIndex(0)
+        }
+      
+    }, 1000);
+   return()=>clearInterval(a)
+    },[index])
+        
+      
+
+    //  let a = 0;
+    //     setInterval(() => {
+    //         a++;
+    //          console.log(a)
+    //         if(a == 5){
+    //             a = 0
+    //         }
+            
+    //     }, 1000);
+       
     return(
         <>
-        <img src={laptopImage} alt="" />
+        <img src={allImages[index]} alt="" />
         </>
     )
 }
